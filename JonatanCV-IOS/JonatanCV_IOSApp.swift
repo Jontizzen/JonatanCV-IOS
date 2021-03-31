6bcfe64b16727577b5b5837f0c9795ac179beaf5
Jontizz <jonatan.larsson77@gmail.com>
2021-03-31 19:59:32 +0200

    Initial Commit

---
+++ JonatanCV-IOS/JonatanCV_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JonatanCV_IOSApp.swift
+//  JonatanCV-IOS
+//
+//  Created by Debora Johansson on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct JonatanCV_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
