4369f35cecd6e9c1d0d6827d3c3e3604a677203b
Jontizz <jonatan.larsson77@gmail.com>
2021-04-02 17:16:22 +0200

    FEAT: Created experience item view

---
+++ JonatanCV-IOS/Experiences/ExperienceItem.swift
@@ -0,0 +1,45 @@
+//
+//  ExperienceItem.swift
+//  JonatanCV-IOS
+//
+//  Created by Debora Johansson on 4/2/21.
+//
+
+import SwiftUI
+
+struct ExperienceItem: View {
+    
+    var experience : Experience
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            experience.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 130, height: 130)
+                .cornerRadius(5)
+            
+            Text(experience.name)
+                .font(.caption)
+                .foregroundColor(.primary)
+                .padding(.leading)
+                
+        }
+    }
+}
+
+struct ExperienceItem_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        
+        Group {
+            
+            ExperienceItem(experience: ModelData().Experiences[4])
+            ExperienceItem(experience: ModelData().Experiences[6])
+
+        }
+    }
+}
