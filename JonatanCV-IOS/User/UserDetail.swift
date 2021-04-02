b77c7d277378857351c21dcc956b5954a1dd93ac
Jontizz <jonatan.larsson77@gmail.com>
2021-04-02 16:40:28 +0200

    FEAT: Added background image and user image to view

--- JonatanCV-IOS/User/UserDetail.swift
+++ JonatanCV-IOS/User/UserDetail.swift
@@ -13,13 +13,40 @@ extension Color {
 }
 
 struct UserDeatil: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).foregroundColor(.newPrimaryColor)
+        
+        ScrollView {
+            
+            ZStack {
+                
+                Image("softwareDevelopment")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 450, height: 300, alignment: .top)
+                    .edgesIgnoringSafeArea(.all)
+                    .padding(.bottom)
+                    .offset(y: -50)
+                
+                Profile.default.image
+                    .resizable()
+                    .frame(width: 230, height: 250, alignment: .bottom)
+                    .padding(.top)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.newPrimaryColor, lineWidth: 3))
+                    .shadow(radius: 7)
+                    .offset(y: 50)
+            }
+            
+            
+        }
     }
 }
 
 struct UserDeatil_Previews: PreviewProvider {
+    
     static var previews: some View {
+        
         UserDeatil()
     }
 }
