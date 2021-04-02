a9e72038e81a1e0ced65914195110c62e4b99c98
Jontizz <jonatan.larsson77@gmail.com>
2021-04-02 17:22:43 +0200

    FEAT: Created circle image helper

---
+++ JonatanCV-IOS/Helpers/CircleImage.swift
@@ -0,0 +1,36 @@
+//
+//  CircleImage.swift
+//  JonatanCV-IOS
+//
+//  Created by Debora Johansson on 4/2/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    
+    var image: Image
+    
+    var body: some View {
+        
+        image
+            .resizable()
+            .renderingMode(.original)
+            .frame(width: 250, height: 250, alignment: .center)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        Group {
+            
+            CircleImage(image: Image("JUImage"))
+            CircleImage(image: Image("HTSImage"))
+        }
+    }
+}
