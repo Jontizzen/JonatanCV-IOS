c26a145088cf97ee7e56b805920a2890bd6c3dd3
Jontizz <jonatan.larsson77@gmail.com>
2021-04-02 13:22:59 +0200

    FEAT: Added modelData as observable object and function to load resources

---
+++ JonatanCV-IOS/Models/ModelData.swift
@@ -0,0 +1,54 @@
+//
+//  ModelData.swift
+//  JonatanCV-IOS
+//
+//  Created by Jonatan Larsson on 4/2/21.
+//
+
+import Foundation
+
+//MARK:- SwiftUI subscribes to your observable object, and updates any views that need refreshing when the data changes.
+final class ModelData: ObservableObject {
+    
+    @Published var Experiences : [Experience] = load("workExperienceData.json")
+
+    // Instance of the user profile that persists even after the user dismisses the profile view
+    //@Published var profile = Profile.default
+    
+    //@Published var profile : Profile = load("profileData.json")
+    
+    var categories: [String: [Experience]] {
+            Dictionary(
+                grouping: Experiences,
+                by: { $0.category.rawValue }
+            )
+    }
+    
+    var features: [Experience] {
+        
+        Experiences.filter { $0.isFeatured }
+    }
+}
+
+func load<T: Decodable>(_ filename: String) -> T {
+    
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Could not find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Could not load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Could not parse \(filename) as \(T.self):\n\(error)")
+    }
+}
